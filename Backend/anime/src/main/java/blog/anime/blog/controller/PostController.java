a54815579f6b6bea139b13f5209ede41e404bb26
Namejoao46 <Namejoao46@gmail.com>
2025-10-21 12:27:34 -0300

    feat: estruturar backend e ajustar versão do Maven
    - Criadas pastas e classes para model, repository, service e controller
    - Implementados endpoints para listar e salvar registros
    - Alterada versão do Maven para 8.0 devido a incompatibilidades com a versão 9

---
+++ Backend/anime/src/main/java/blog/anime/blog/controller/PostController.java
@@ -0,0 +1,33 @@
+package blog.anime.blog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import blog.anime.blog.model.Post;
+import blog.anime.blog.service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+@CrossOrigin(origins = "*")
+public class PostController {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping
+    public List<Post> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Post salvar(@RequestBody Post post) {
+        return service.salvar(post);
+    }
+}
---
+++ Backend/anime/src/main/java/blog/anime/blog/model/Post.java
@@ -0,0 +1,30 @@
+package blog.anime.blog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String conteudo;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getConteudo() { return conteudo; }
+    public void setConteudo(String conteudo){ this.conteudo = conteudo; }
+
+    
+
+    
+    
+}
